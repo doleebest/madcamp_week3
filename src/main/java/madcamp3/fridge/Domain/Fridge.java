07ec6956aab5722ffe26f6ea46a366bee9bdd2a7
doleebest <sj27419@gmail.com>
2025-01-11 21:22:27 +0900

    temp

--- src/main/java/madcamp3/fridge/Domain/Fridge.java
+++ src/main/java/madcamp3/fridge/Domain/Fridge.java
@@ -1,22 +1,35 @@
 package madcamp3.fridge.Domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "fridges")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Fridge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name; // 냉장고 이름
+    private String userId;  // 카카오 사용자 ID
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @OneToMany(mappedBy = "fridge", cascade = CascadeType.ALL)
+    private List<DetectedItem> items = new ArrayList<>();
 
-    @OneToMany(mappedBy = "fridge", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<DetectedItem> detectedItems = new ArrayList<>();
+    @Builder
+    public Fridge(String userId) {
+        this.userId = userId;
+    }
+
+    public void addItem(DetectedItem item) {
+        this.items.add(item);
+        item.setFridge(this);
+    }
 }
