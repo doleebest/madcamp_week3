d1790a74dcd22b493c545688ce1dd4a4805e23bd
doleebest <sj27419@gmail.com>
2025-01-15 17:09:01 +0900

    fix params with userEmail

--- src/main/java/madcamp3/fridge/Controller/UserController.java
+++ src/main/java/madcamp3/fridge/Controller/UserController.java
@@ -11,28 +11,20 @@ import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api")  // API 기본 경로
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
 
     private final SecurityUtil securityUtil;
     private final UserPhoneNumberService userPhoneNumberService;
 
-
-    // CORS 설정: localhost:3000에서 요청 허용
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/user")
-    public ResponseEntity<User> getUser() {
-        User user = securityUtil.getCurrentUser();
-        return ResponseEntity.ok(user);
-    }
-
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "*")  // 안드로이드와 통신을 위해 CORS 설정 변경
     @PostMapping("/user/parents-phone")
-    public ResponseEntity<UserPhoneNumber> saveParentsPhone(@RequestBody ParentsPhoneRequest request) {
-        User currentUser = securityUtil.getCurrentUser();
+    public ResponseEntity<UserPhoneNumber> saveParentsPhone(
+            @RequestParam String userEmail,  // URL 파라미터로 userEmail 받기
+            @RequestBody ParentsPhoneRequest request) {
         UserPhoneNumber savedNumber = userPhoneNumberService.saveParentsPhone(
-                currentUser.getEmail(),
+                userEmail,  // URL에서 받은 이메일 사용
                 request.getParentsPhone()
         );
         return ResponseEntity.ok(savedNumber);
